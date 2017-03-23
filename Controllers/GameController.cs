551f2b50df40bbf87344af303bc9cf8fef641f31
EJ Hammond <ejh891@gmail.com>
2017-03-22 22:19:18 -0400

    Add GameController

---
+++ Controllers/GameController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretHiplerServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameController : Controller
+    {
+        // POST game/{gameid}
+        [HttpPost]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
